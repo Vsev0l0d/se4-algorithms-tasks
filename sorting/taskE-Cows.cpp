e275e2b2f0647dcfa04990f4faa8359878fd6d16
Vsev0l0d <vsevolod628@gmail.com>
2021-03-20 22:53:23 +0300

    Hooray

--- sorting/taskE-Cows.cpp
+++ sorting/taskE-Cows.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 
 int main() {
-    cout << "sex" << endl;
+    int n, k;
+    cin >> n >> k;
+    int64_t coordinates[n];
+    for (int i = 0; i < n; i++) cin >> coordinates[i];
+
+    int64_t left = 0;
+    int64_t right = coordinates[n-1] - coordinates[0] + 1;
+    while(left < right){
+        int64_t middle = (left + right) >> 1;
+        int64_t last = coordinates[0];
+        int count = 1;
+        for (int i = 1; i < n; i++){
+            if (coordinates[i] - last > middle){
+                last = coordinates[i];
+                count++;
+            }
+        }
+        if (count >= k) left = middle + 1;
+        else right = middle;
+    }
+    cout << left << endl;
     return 0;
 }
--- sorting/taskF-Number.cpp
+++ sorting/taskF-Number.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <set>
 using namespace std;
 
 int main() {
