62524125d3f1231969b1ae6725f848d831d929f4
Vsev0l0d <vsevolod628@gmail.com>
2021-03-21 21:24:49 +0300

    Hooray

--- sorting/taskF-Number.cpp
+++ sorting/taskF-Number.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
+bool compare(const string& x1, const string& x2){return (x1 + x2 > x2 + x1);}
+
 int main() {
-    cout << "sex" << endl;
+    list<string> data;
+    string x;
+    while (cin >> x) data.push_back(x);
+    data.sort(compare);
+    for(const string& x: data) cout << x;
     return 0;
 }
