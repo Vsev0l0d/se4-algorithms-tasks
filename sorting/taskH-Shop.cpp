e739f7ea49240abff5a8c32aae5a138990e000c2
Vsev0l0d <vsevolod628@gmail.com>
2021-03-22 20:43:06 +0300

    Hooray

--- sorting/taskH-Shop.cpp
+++ sorting/taskH-Shop.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    cout << "sex" << endl;
+    int n, k;
+    cin >> n >> k;
+    int prices[n];
+    for (int i = 0; i < n; i++) cin >> prices[i];
+    sort(prices, prices + n, greater<int>());
+    int32_t total_price = 0;
+    for (int i = 0; i < n; i++)
+        if((i + 1) % k != 0) total_price += prices[i];
+    cout << total_price << endl;
     return 0;
 }
