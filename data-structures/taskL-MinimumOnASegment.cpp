a38f549a0572a2c09605a25e29de65ff8a4bc085
Vsev0l0d <vsevolod628@gmail.com>
2021-04-09 19:03:47 +0300

    Hooray

--- data-structures/taskL-MinimumOnASegment.cpp
+++ data-structures/taskL-MinimumOnASegment.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
 int main() {
-    cout << "sex" << endl;
+    int n, k, x;
+    cin >> n >> k;
+    list <pair<int, int>> segment;
+    for (int i = 0; i < n; i++){
+        if (!segment.empty() && i - segment.front().first >= k) segment.pop_front();
+        cin >> x;
+        while (!segment.empty() && segment.back().second > x) segment.pop_back();
+        segment.push_back({i, x});
+        if (i + 1 >= k) cout << segment.front().second << " ";
+    }
     return 0;
 }
