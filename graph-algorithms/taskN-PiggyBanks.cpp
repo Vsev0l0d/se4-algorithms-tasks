75ac0783e22978d52aeafe391fb8523914361790
Vsev0l0d <vsevolod628@gmail.com>
2021-05-23 18:17:10 +0300

    Hooray

--- graph-algorithms/taskN-PiggyBanks.cpp
+++ graph-algorithms/taskN-PiggyBanks.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
+void dfs(int v, pair<list<int>, int>* graph, int * count){
+    graph[v].second = 1;
+    for(int next : graph[v].first){
+        if (graph[next].second == 0) dfs(next, graph, count);
+        else if (graph[next].second == 1) (*count)++;
+    }
+    graph[v].second = 2;
+}
+
 int main() {
-    cout << "sex" << endl;
+    int n, key, count = 0;
+    cin >> n;
+    pair<list<int>, int> graph[n];
+    for (int i = 0; i < n; i++) {
+        cin >> key;
+        graph[--key].first.push_back(i);
+    }
+
+    for (int i = 0; i < n; i++)
+        if (graph[i].second == 0) dfs(i, graph, &count);
+    cout << count;
     return 0;
 }
